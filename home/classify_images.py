7860fd5c1b62158378830397801f3e0d3f14f6ff
willdata0101 <willthelion@outlook.com>
2020-05-06 15:26:53 -0500

    First commit

---
+++ home/classify_images.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
+#                                                                             
+# PROGRAMMER: 
+# DATE CREATED:                                 
+# REVISED DATE: 
+# PURPOSE: Create a function classify_images that uses the classifier function 
+#          to create the classifier labels and then compares the classifier 
+#          labels to the pet image labels. This function inputs:
+#            -The Image Folder as image_dir within classify_images and function 
+#             and as in_arg.dir for function call within main. 
+#            -The results dictionary as results_dic within classify_images 
+#             function and results for the functin call within main.
+#            -The CNN model architecture as model wihtin classify_images function
+#             and in_arg.arch for the function call within main. 
+#           This function uses the extend function to add items to the list 
+#           that's the 'value' of the results dictionary. You will be adding the
+#           classifier label as the item at index 1 of the list and the comparison 
+#           of the pet and classifier labels as the item at index 2 of the list.
+#
+##
+# Imports classifier function for using CNN to classify images 
+from classifier import classifier 
+
+# TODO 3: Define classify_images function below, specifically replace the None
+#       below by the function definition of the classify_images function. 
+#       Notice that this function doesn't return anything because the 
+#       results_dic dictionary that is passed into the function is a mutable 
+#       data type so no return is needed.
+# 
+def classify_images(images_dir, results_dic, model):
+    for key in results_dic:
+        model_label = ""
+        model_label = classifier(images_dir + key, model)
+        model_label = model_label.lower()
+        resultList = [x.strip() for x in model_label.split(',')]
+    
+        truth = results_dic[key][0]
+        
+        
+        Match = 0
+        for x in (resultList):
+            if (x == truth):
+                Match = 1
+
+        results_dic[key].extend([model_label, Match])
+        
+        
+            
+    print(results_dic)
---
+++ home/get_pet_labels.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
+#                                                                             
+# PROGRAMMER: Will
+# DATE CREATED:                                  
+# REVISED DATE: 
+# PURPOSE: Create the function get_pet_labels that creates the pet labels from 
+#          the image's filename. This function inputs: 
+#           - The Image Folder as image_dir within get_pet_labels function and 
+#             as in_arg.dir for the function call within the main function. 
+#          This function creates and returns the results dictionary as results_dic
+#          within get_pet_labels function and as results within main. 
+#          The results_dic dictionary has a 'key' that's the image filename and
+#          a 'value' that's a list. This list will contain the following item
+#          at index 0 : pet image label (string).
+#
+##
+# Imports python modules
+from os import listdir
+
+# TODO 2: Define get_pet_labels function below please be certain to replace None
+#       in the return statement with results_dic dictionary that you create 
+#       with this function
+# 
+def get_pet_labels(image_dir):
+    in_files = listdir(image_dir)
+    results_dic = dict()
+    for idx in range(0, len(in_files), 1):
+        if in_files[idx][0] != ".":
+            pet_label = ""
+            low_in_files = in_files[idx].lower()
+            word_list_in_files = low_in_files.split("_")
+            for word in word_list_in_files:
+                if word.isalpha():
+                    pet_label += word + " "
+            pet_label = pet_label.strip()
+            
+            if in_files[idx] not in results_dic:
+              results_dic[in_files[idx]] = [pet_label]
+            
+        else:
+            print("** Warning: Key=", filenames[idx], "already exists in results_dic with value =", results_dic[filenames[idx]])
+    print("\nPrinting all key-value pairs in dictionary results_dic:")
+    for key in results_dic:
+        print("Filename=", key, " Pet Label=", results_dic[key][0])
+    # Replace None with the results_dic dictionary that you created with this
+    # function
+    return results_dic
